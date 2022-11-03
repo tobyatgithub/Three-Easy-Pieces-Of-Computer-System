250a1bace15b9f7b1e1e5d7d16bb1a56369370d1
tobyatgithub <toby.fangyuan@gmail.com>
2022-11-03 16:08:59 -0700

    update evic policy to add more test cases with different capacity.

--- eviction/main.cpp
+++ eviction/main.cpp
@@ -6,15 +6,28 @@ int main() {
     vector<int> jobList = {1,2,3,4,5,6,7,1,2,3,4,5,6,7};
     // test 1, FIFO
     SwapManager FIFO = SwapManager(5, "FIFO");
+    SwapManager FILO = SwapManager(5, "FILO");
+    std::cout << "Testing FIFO with cap = 5" << std::endl;
     for( int job: jobList) {
         FIFO.get(job);
     }
-
-    SwapManager FILO = SwapManager(5, "FILO");
+    std::cout << "Testing FILO with cap = 5" << std::endl;
     for( int job: jobList) {
         FILO.get(job);
     }
 
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Change capacity to 7!" << std::endl;
+
+    std::cout << "Testing FIFO with cap = 7" << std::endl;
+    FIFO.setCapacity(7);
+    for( int job: jobList) {
+        FIFO.get(job);
+    }
+
+    std::cout << "Testing FILO with cap = 7" << std::endl;
+    FILO.setCapacity(7);
+    for( int job: jobList) {
+        FILO.get(job);
+    }
     return 0;
 }
